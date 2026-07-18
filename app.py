990ac0e409570d80c33dcccbbe472f114b2b9b8d
karen0510-eng <karenag0510@gmail.com.com>
2026-07-18 12:21:41 -0600

    Configurar Vercel

--- app.py
+++ app.py
@@ -134,5 +134,5 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
